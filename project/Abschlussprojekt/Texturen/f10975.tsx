f21f38542fb72c7ad520b066d27b57ffd9ca1206
Meoli <mertens.oliver@gmx.de>
2021-08-19 14:43:03 +0200

    neue Texturen

---
+++ project/Abschlussprojekt/Texturen/f10975.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="f10975" tilewidth="16" tileheight="16" tilecount="171" columns="19">
+ <image source="../../../../../../../../../Downloads/f10975.png" width="308" height="145"/>
+</tileset>
